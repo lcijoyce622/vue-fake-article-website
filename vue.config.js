d6f756817acec8d4c6183b25fe4baac885cedcc5
lcijoyce622 <lcijoyce622@gmail.com>
2021-05-14 23:50:55 +0800

    fix module

--- vue.config.js
+++ vue.config.js
@@ -1,32 +1,33 @@
- {
-    module: {
-        rules: [
-            {  publicPath: process.env.NODE_ENV === 'production' ? '/vue-fake-article-website/' : '/'
-        },
-            { test: /\.scss$/ },
-            {
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            data: `
-                        @import "@/scss/_variables.scss";
-                        @import "@/scss/_mixins.scss";
-                        `,
+module.exports = {
+    // module: {
+    //     rules: [
+            // {
+                  publicPath: process.env.NODE_ENV === 'production' ? '/vue-fake-article-website/' : '/'
+        // },
+        //     { test: /\.scss$/ },
+        //     {
+        //         use: [
+        //             'vue-style-loader',
+        //             'css-loader',
+        //             {
+        //                 loader: 'sass-loader',
+        //                 options: {
+        //                     data: `
+        //                 @import "@/scss/_variables.scss";
+        //                 @import "@/scss/_mixins.scss";
+        //                 `,
 
-                        }
-                    }
-                ]
-            }
-        ]
+        //                 }
+        //             }
+        //         ]
+        //     }
+        // ]
     
 }
 
 
 
 
-}
+// }
 
 
